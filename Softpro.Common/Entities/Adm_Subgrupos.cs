cc49fed1d8e16d06f05553b54a6b1af321ee29eb
Carmelo Gonzalez <Carmelo_Gonzalez@wgresorts.com>
2021-08-14 13:17:28 -0400

    DbContex

---
+++ Softpro.Common/Entities/Adm_Subgrupos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Softpro.Common.Entities
+{
+    public class Adm_Subgrupos
+    {
+        [Key]
+        [MaxLength(4, ErrorMessage = "El campo {0} solo puede tener un maximo de {1} caracteres")]
+        public string  SubgrupoID { get; set; }
+
+        [MaxLength(250, ErrorMessage = "El campo {0} solo puede tener un maximo de {1} caracteres")]
+        [Display(Name = "Nombre del subgrupo")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string  Subgrupo { get; set; }
+    }
+}
---
+++ Softpro.Web/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Softpro.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        //public DataContext(DbContextOptions<DataContext> options) : base(options)
+        //{
+        //}
+
+        //public DbSet<Country> Countries { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+
+        //    modelBuilder.Entity<Country>()
+        //        .HasIndex(t => t.Name)
+        //        .IsUnique();
+        //}
+    }
+
+}
